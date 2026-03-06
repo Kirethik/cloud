4cced98532bb0fb41ef1283e266abb2c0bcc56c5
Kirethik <mohanasundaramdeepa@gmail.com>
2026-03-06 23:06:21 +0530

    Hardcode API URL to Azure Backend and push Redis removal fixes

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// Defaults to localhost if env API URL not set
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api/v1';
+const API_URL = 'https://ecommerce-backend.azurewebsites.net/api/v1';
 
 export const api = axios.create({
     baseURL: API_URL,
