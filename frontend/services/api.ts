ab4d71e0a20712ad8e1ed5327bb87fd67f4292cf
Kirethik <mohanasundaramdeepa@gmail.com>
2026-03-06 23:13:42 +0530

    Update API URL to correct backend endpoint

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://ecommerce-backend.azurewebsites.net/api/v1';
+const API_URL = 'https://ecommerce-backend-afb9hdd0hjhvhcdq.centralindia-01.azurewebsites.net/api/v1';
 
 export const api = axios.create({
     baseURL: API_URL,
